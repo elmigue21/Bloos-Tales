8a5036a664e5046b29ac30fa471a5d88a842bfdc
elmigue21 <gueljohnc@gmail.com>
2024-05-10 17:49:58 +0800

    ererererer

--- ComProg2Finals/Form1.Designer.cs
+++ ComProg2Finals/Form1.Designer.cs
@@ -32,18 +32,12 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button6 = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -88,21 +82,11 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(695, 184);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(93, 45);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Knight";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(179, 52);
+            this.label1.Location = new System.Drawing.Point(75, 52);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(84, 13);
             this.label1.TabIndex = 5;
@@ -111,26 +95,16 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(548, 37);
+            this.pictureBox1.Location = new System.Drawing.Point(639, 75);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(100, 97);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(693, 236);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(95, 45);
-            this.button6.TabIndex = 7;
-            this.button6.Text = "Wizard";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(61, 52);
+            this.pictureBox2.Location = new System.Drawing.Point(78, 75);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(100, 97);
             this.pictureBox2.TabIndex = 8;
@@ -140,7 +114,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(179, 75);
+            this.label2.Location = new System.Drawing.Point(75, 175);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(87, 13);
             this.label2.TabIndex = 9;
@@ -150,7 +124,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(458, 37);
+            this.label3.Location = new System.Drawing.Point(636, 52);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(84, 13);
             this.label3.TabIndex = 10;
@@ -160,51 +134,11 @@
             // 
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label4.Location = new System.Drawing.Point(455, 52);
+            this.label4.Location = new System.Drawing.Point(636, 175);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(87, 13);
             this.label4.TabIndex = 11;
             this.label4.Text = "Character Health";
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(334, 203);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(132, 45);
-            this.button7.TabIndex = 12;
-            this.button7.Text = "Next Turn";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(693, 287);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(95, 45);
-            this.button8.TabIndex = 13;
-            this.button8.Text = "Priest";
-            this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
-            // 
-            // button9
-            // 
-            this.button9.Location = new System.Drawing.Point(695, 338);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(95, 45);
-            this.button9.TabIndex = 14;
-            this.button9.Text = "Rogue";
-            this.button9.UseVisualStyleBackColor = true;
-            this.button9.Click += new System.EventHandler(this.button9_Click);
-            // 
-            // button10
-            // 
-            this.button10.Location = new System.Drawing.Point(693, 389);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(95, 45);
-            this.button10.TabIndex = 15;
-            this.button10.Text = "Arcehr";
-            this.button10.UseVisualStyleBackColor = true;
-            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // Form1
             // 
@@ -212,24 +146,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button10);
-            this.Controls.Add(this.button9);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.button6);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
@@ -243,18 +172,12 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button6;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button10;
     }
 }
 
--- ComProg2Finals/Form1.cs
+++ ComProg2Finals/Form1.cs
@@ -18,10 +18,9 @@ namespace ComProg2Finals
         List<Character> characters;
         string directory;
 
-
         Character[] turns { get; set; }
         Character currentTurn { get; set; }
-        Character enemy { get; set; }
+        Character Enemy { get; set; }
 
 
         public Form1()
@@ -29,17 +28,16 @@ namespace ComProg2Finals
             InitializeComponent();
 
 
-            characters = new List<Character>();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
 
             Knight soulknight = new Knight("Soul Knight");
             currentNpc = soulknight;
-            Wizard wizard = new Wizard("GANDALF THE GREY");
-            
-
-            Priest priest = new Priest("RAFAELLA");
-            Rogue rogue = new Rogue("miROGUEl");
-            Archer archer = new Archer("legolas");
-
 
 
             Bloo bloo = new Bloo("Bloo");
@@ -50,23 +48,7 @@ namespace ComProg2Finals
 
             label1.ForeColor = Color.White;
 
-            characters.Add(soulknight);
-            characters.Add(wizard);
-            characters.Add(priest);
-            characters.Add(rogue);
-            characters.Add(archer);
-
-
-
-            /*
-            characters[0] = soulknight;
-            characters[1] = wizard;
-            characters[2] = priest;
-            characters[3] = rogue;
-            characters[4] = archer;
-            */
-
-
+            //characters.Add(soulknight);
 
             label3.Text = soulknight.Name;
 
@@ -80,33 +62,45 @@ namespace ComProg2Finals
             pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
 
 
-            
+            /*
             button1.Text = currentNpc.CharSkills[0].Name;
             button2.Text = currentNpc.CharSkills[1].Name;
             button3.Text = currentNpc.CharSkills[2].Name;
             button4.Text = currentNpc.CharSkills[3].Name;
+            */
 
+
+            Enemy = soulknight;
+
+            Enemy.Opposition = Player;
+            Player.Opposition = Enemy;
+
+            currentTurn = Enemy;
+            //currentTurn = Player;
+            runTurn();
 
 
         }
-        private void updateLabels()
+        private void runTurn()
         {
-            label3.Text = currentNpc.Name;
-            pictureBox1.Image = Image.FromFile(Path.Combine(directory, "assets", currentNpc.Image));
+            for(int i = 0; i < currentTurn.CharStatEffects.Count; i++)
+            {
+                currentTurn.CharStatEffects[i].Trigger(currentTurn);
+            }
 
-            button1.Text = currentNpc.CharSkills[0].Name;
-            button2.Text = currentNpc.CharSkills[1].Name;
-            button3.Text = currentNpc.CharSkills[2].Name;
-            button4.Text = currentNpc.CharSkills[3].Name;
 
-            label2.Text = "Health: " + Player.Health.ToString();
-        }
 
-        private void button5_Click(object sender, EventArgs e)
-        {
-            currentNpc = characters[0];
-            updateLabels();
 
+                label1.Text = Player.Name;
+                label2.Text = "Health: " + Player.Health.ToString();
+                label3.Text = Enemy.Name;
+                label4.Text = "Health: " + Enemy.Health.ToString();
+
+
+                button1.Text = currentTurn.CharSkills[0].Name;
+                button2.Text = currentTurn.CharSkills[1].Name;
+                button3.Text = currentTurn.CharSkills[2].Name;
+                button4.Text = currentTurn.CharSkills[3].Name;         
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -117,57 +111,61 @@ namespace ComProg2Finals
         private void button1_Click(object sender, EventArgs e)
         {
 
-            currentNpc.UseSkill1(Player);
-            label2.Text = "Health: " + Player.Health.ToString();
+            currentTurn.UseSkill1(currentTurn.Opposition);
+            currentTurn = currentTurn.Opposition;
+            if (Player.Health <= 0)
+            {
+                MessageBox.Show("Fight has ended");
+            }
+            else
+            {
+                runTurn();
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            
-            currentNpc.UseSkill2(Player);
-            label2.Text = "Health: " + Player.Health.ToString();
+
+            currentTurn.UseSkill2(currentTurn.Opposition);
+            currentTurn = currentTurn.Opposition;
+            if (Player.Health <= 0)
+            {
+                MessageBox.Show("Fight has ended");
+            }
+            else
+            {
+                runTurn();
+            }
+
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            currentNpc.UseSkill3(Player);
-            label2.Text = "Health: " + Player.Health.ToString();
+            currentTurn.UseSkill3(currentTurn.Opposition);
+            currentTurn = currentTurn.Opposition;
+            if (Player.Health <= 0)
+            {
+                MessageBox.Show("Fight has ended");
+            }
+            else
+            {
+                runTurn();
+            }
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            currentNpc.UseSkill4(Player);
-            label2.Text = "Health: " + Player.Health.ToString();
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            currentNpc = characters[1];
-            updateLabels();
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            for(int i =0; i < Player.CharStatEffects.Count; i++)
+            currentTurn.UseSkill4(currentTurn.Opposition);
+            currentTurn = currentTurn.Opposition;
+            if (Player.Health <= 0 )
             {
-                Player.CharStatEffects[i].Trigger(Player);
+                MessageBox.Show("Fight has ended");
             }
-            updateLabels();
+            else
+            {
+                runTurn();
+            }
         }
 
-        private void button8_Click(object sender, EventArgs e)
-        {
-            currentNpc = characters[2]; updateLabels();
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-            currentNpc = characters[3]; updateLabels();
-        }
-
-        private void button10_Click(object sender, EventArgs e)
-        {
-            currentNpc = characters[4]; updateLabels();
-        }
     }
 }
