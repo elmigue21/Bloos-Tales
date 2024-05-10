6ba5dce454b6da438add0d4d67ae556b56a7b415
elmigue21 <gueljohnc@gmail.com>
2024-05-10 19:27:02 +0800

    dafhtine

---
+++ ComProg2Finals/Events.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ComProg2Finals
+{
+    public class Events
+    {
+        public virtual void Perform(Character target)
+        {
+            MessageBox.Show($"Events");
+
+        }
+    }
+    public class Chest : Events
+    {
+        public override void Perform(Character target)
+        {
+            MessageBox.Show($"Chest");
+        }
+    }
+    public class Cave : Events
+    {
+
+    }
+}
---
+++ ComProg2Finals/Items.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComProg2Finals
+{
+    public class Items
+    {
+        public virtual void Perform(Character target)
+        {
+            MessageBox.Show($"Items");
+
+        }
+    }
+    public class Rock : Items
+    {
+        public override void Perform(Character target)
+        {
+            MessageBox.Show($"Rock");
+        }
+    }
+    public class SacrificeRing : Items
+    {
+        public override void Perform(Character target)
+        {
+            MessageBox.Show($"Sacrifice Ring");
+        }
+    }
+}
