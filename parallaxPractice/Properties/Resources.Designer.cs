9d8e33be6df400220222e14746abda65a5d3d041
Aiphie <sbmagkasi@gmail.com>
2024-05-25 17:42:09 +0800

    Embed Font

--- parallaxPractice/Properties/Resources.Designer.cs
+++ parallaxPractice/Properties/Resources.Designer.cs
@@ -241,6 +241,16 @@ namespace parallaxPractice.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] PressStart2P_Regular {
+            get {
+                object obj = ResourceManager.GetObject("PressStart2P_Regular", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap scroll {
